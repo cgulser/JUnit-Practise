31ff9300d856752b869e491efa631ba5b5d0be2e
Cevdet Gulser <c.gulser86@gmail.com>
2022-05-24 19:02:27 +0200

    version

--- src/test/java/practice/Q09_Alert.java
+++ src/test/java/practice/Q09_Alert.java
@@ -1,7 +1,9 @@
 package practice;
-
-public class Q09_Alert {
-
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+public class Q09_Alert extends TestBase {
     //    go to url :http://demo.automationtesting.in/Alerts.html
     //    click  "Alert with OK" and click 'click the button to display an alert box:'
     //    accept Alert(I am an alert box!) and print alert on console
@@ -10,6 +12,25 @@ public class Q09_Alert {
     //    click "Alert with Textbox" and click 'click the button to demonstrate the prompt box'
     //    and then sendKeys 'TechProEducation' (Please enter your name)
     //    finally print on console this message "Hello TechproEducation How are you today"
-
-
+    //    aseertion these message
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("http://demo.automationtesting.in/Alerts.html");
+        driver.findElement(By.xpath("(//a[@class='analystic'])[1]")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-danger']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        driver.findElement(By.xpath("//*[text()='Alert with OK & Cancel ']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+        driver.findElement(By.xpath("(//a[@class='analystic'])[3]")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().sendKeys("TechProEducation");
+        driver.switchTo().alert().accept();
+        String expectedMsj = "Hello TechProEducation How are you today";
+        String actualMsj = driver.findElement(By.xpath("//p[@id='demo1']")).getText();
+        Assert.assertEquals(expectedMsj, actualMsj);
+    }
 }
--- src/test/java/practice/Q10_Iframe.java
+++ src/test/java/practice/Q10_Iframe.java
@@ -1,13 +1,41 @@
 package practice;
-
-public class Q10_Iframe  {
-      /*
-    // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
-    // web sitesini maximize yapin
-    // ikinci emojiye tıklayın
-    // tüm ikinci emoji öğelerini tıklayın
-    // parent iframe e geri donun
-    // formu doldurun,(Formu istediğiniz metinlerle doldurun)
-    //  apply button a basin
- */
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Q10_Iframe extends TestBase {
+    /*
+  // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
+  // web sitesini maximize yapin
+  // ikinci emojiye tıklayın
+  // tüm ikinci emoji öğelerini tıklayın
+  // parent iframe e geri donun
+  // formu doldurun,(Formu istediğiniz metinlerle doldurun)
+  //  apply button a basin
+*/
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id ='emoojis']"));
+        driver.switchTo().frame(iframe);
+        WebElement secondEmoji = driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
+        secondEmoji.click();
+        List<WebElement> emojiOgeler = driver.findElements(By.xpath("//div[@id='nature']//div//img"));
+        // emojiOgeler.stream().forEach(x-> x.click());
+        for(WebElement each : emojiOgeler){
+            each.click();
+        }
+        Thread.sleep(3000);
+        driver.switchTo().parentFrame();
+        List<WebElement> textList = driver.findElements(By.xpath("//input[@class='mdl-textfield__input']"));
+        List<String> text = new ArrayList<>(Arrays.asList("Bir", "iframe" , "sorusu", "bu", "kadar", "eglenceli", "olabilir","sizce de ", "oyle degil mi ?", "",""));
+        for (int i=0; i< textList.size();i++){
+            textList.get(i).sendKeys(text.get(i));
+        }
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@id='send']")).click();
+    }
 }
