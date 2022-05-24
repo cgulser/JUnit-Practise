747d2d8b835ccad958107a3812024731f0a2d31f
Cevdet Gulser <c.gulser86@gmail.com>
2022-05-24 17:36:35 +0200

    Initial commit

---
+++ src/test/java/java/day10_actions/C01_TestBaseFirstClass.java
@@ -0,0 +1,12 @@
+package java.day10_actions;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C01_TestBaseFirstClass extends TestBase {
+
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+    }
+}
